80a8e456fb6f578a2f5e97adfc08cae7f2957447
05NishanRana <nishanr663@gmail.com>
2025-02-24 15:02:51 +0545

    added Hello World

---
+++ pp.py
@@ -0,0 +1,101 @@
+# def calculategmean (a,b):
+#     mean = (a*b)/(a+b)
+#     print (mean)
+
+
+
+
+# a = 9 
+# b = 8 
+# if a>b:
+#     print("frist number is greater ")
+# else:
+#     print("second number is greater")    
+
+
+# calculategmean(a,b)
+# # print (gmean)
+# c = 8
+# d = 7
+
+# # gmean2 = (c*d)/(c+d)
+# # print(gmean2)
+# calculategmean(c,d)
+
+# def average(a,b):
+    # print("the average is ",(a+b)/2)
+
+# a = 0
+# b = 0
+# print(a > b)
+
+
+# thistuple = ("apple")
+# print(type(thistuple))
+
+# myset = {"apple",1234,"Mango","phone"}
+# for x in myset:
+#     print(x)
+
+# thisset ={"apple","banana","cherrry"}
+# thisset.discard("banana")
+# print(thisset)
+
+# setA = {1,2,3,4,5}
+# setB = {3,4,5,6,7}
+
+# print("union:",setA|setB)
+
+# thisdict = {
+#     "brand":"ford",
+#     "model":"Mustang",
+#     "year":1964
+# }
+# print(thisdict)
+
+# marks = {"python":87,
+#     "OS":80,
+#     "DB":70}
+# print(marks["python"])
+# print(marks["java"])
+# print(marks.get("DB"))
+# print(marks.get("java"))
+
+# out = open ("output .txt","w")
+# out.write("Hello, world!\n")
+# out.write("How are you?")
+# out.close()
+
+# abc = open("output.txt", "r")
+# print(abc.read())
+# print(abc.readlines())
+# for data in abc:
+    # print(data)
+# abc.close()    
+
+# file = open('file3.txt')
+# count = 0
+# for line in file:
+#     count = count + 1
+# print('line count:',count)    
+
+# output:
+# line count: 8
+
+
+# low = int(input("enter the lower bound of range:"))
+# high =int(input("enter the upper bond of the range:"))
+# guess = int(input("enter your guess:"))
+# if low<=guess<=high:
+#     print(f"the number {guess}is within the range{low}-{high}")
+# else:
+#     print(f"the number {guess}is not within the range{low}-{high}")    
+
+thisdict ={
+    "name":"Nishan Rana",
+    "age": 21,
+    "address":"palpa"
+}
+x = thisdict.keys()
+print(x)
+print("Hello World")
